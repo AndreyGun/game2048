5822200f82d561b90290e5c3d3a706a36d6a6f5d
Andrey <andreygunkin90@gmail.com>
2019-08-20 20:21:32 +0300

    test

--- src/components/cell-item/cell-item.js
+++ src/components/cell-item/cell-item.js
@@ -4,8 +4,10 @@ import './cell-item.css'
 const CellItem = (props) => {
 
     const { cellProps, activeItem, value } = props;
+    
     let itemValue = null;
     let cellClass = 'cell-item';
+
     if (cellProps.activeCells.includes(activeItem)) {
         cellClass += ' is-active value-' + value;
         itemValue = value;
